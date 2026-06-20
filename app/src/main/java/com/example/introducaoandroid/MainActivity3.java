baa4bbb5e90b817e67020c378d5e8e491a69db72
Carlos Eduardo <carloslopes4657@gmail.com>
2026-06-20 01:45:58 -0300

    app completo

--- app/src/main/java/com/example/introducaoandroid/MainActivity3.java
+++ app/src/main/java/com/example/introducaoandroid/MainActivity3.java
@@ -1,6 +1,8 @@
 package com.example.introducaoandroid;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,38 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    Button btnOuvir, btnVoltar;
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main3);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnOuvir = findViewById(R.id.btn_ouvir);
+        btnVoltar = findViewById(R.id.btn_voltar);
+
+        btnOuvir.setOnClickListener(view -> tocarSom(R.raw.audi_a3));
+        btnVoltar.setOnClickListener(view -> finish());
+    }
+
+    private void tocarSom(int som) {
+        if (mediaPlayer != null) mediaPlayer.release();
+
+        mediaPlayer = MediaPlayer.create(this, som);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) mediaPlayer.release();
     }
 }
